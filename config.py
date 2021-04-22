1195b3c103f0b710f3250a7ebf09dc95243ac579
Grace Hawkins <amoomajid99@gmail.com>
2021-04-22 15:07:23 +0430

    Add local config

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ COURSES_DIR = os.path.join(DATA_DIR, 'courses')
 LISTENERS_DIR = os.path.join(DATA_DIR, 'listeners')
 
 BOT_TOKEN = '1687090378:AAEEsA5nUI7gttTv6udi-xmomJkeI0B2JUc'
+
+try:
+    from .local_config import *
+except:
+    pass
