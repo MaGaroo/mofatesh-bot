acc5c69a61b4d238f81e8f213357896456efb430
Grace Hawkins <amoomajid99@gmail.com>
2021-10-13 20:45:50 +0200

    Run collector in a different thread

--- collector/__main__.py
+++ collector/__main__.py
@@ -1,4 +1,5 @@
 import time
+import threading
 
 import config
 from collector.crawler import Crawler
@@ -7,8 +8,9 @@ from collector.crawler import Crawler
 def main():
     while True:
         crawler = Crawler()
-        crawler.crawl()
+        threading.Thread(target=crawler.crawl).start()
         time.sleep(config.COLLECTOR_SLEEP_TIME)
+    return 0
 
 
 if __name__ == '__main__':
